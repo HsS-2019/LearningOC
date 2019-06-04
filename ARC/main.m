3aa85a491465f2d2712eb003c525e707084c173c
HsStu <944722664@qq.com>
2019-06-04 20:43:03 +0800

    init the LearningOC Project

---
+++ ARC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ARC
+//
+//  Created by tidot on 2019/6/4.
+//  Copyright © 2019 tidot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, ARC!");
+    }
+    return 0;
+}
